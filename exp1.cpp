56e3ac67bac9de892953cbfaed8cd6ef6fc2aca5
varunp28 <221011463+varunp28@users.noreply.github.com>
2025-10-03 11:29:47 +0530

    Create exp1.cpp

---
+++ exp1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+int main() {
+    int size;
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+/*
+Output:
+Enter the number of elements in the array: 6
+Enter 6 elements: 64 25 12 22 11 90
+Sorted array: 11 12 22 25 64 90
+*/
